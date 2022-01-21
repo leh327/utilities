caf9b2a7f3276c54d921de4ba02510f02230c99c
leh327 <leh327@yahoo.com>
2022-01-21 10:17:26 -0500

    Create remove-terminating-namespace.sh

---
+++ remove-terminating-namespace.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+echo Enter name of namespace
+read namesapcename
+#https://craignewtondev.medium.com/how-to-fix-kubernetes-namespace-deleting-stuck-in-terminating-state-5ed75792647e
+kubectl get namespace ${namespacename} -o json > /tmp/${namespacename}.json
+
+cat>replace-finalizer.yml<<EOF
+- hosts: localhost
+  gather_facts: no
+  tasks:
+  - replace:
+      path: "/tmp//tmp/{{namespacename}}.json"
+      regexp: |
+        "spec": {
+            "finalizers": [
+                "kubernetes"
+            ]
+        },
+      replace: |
+        "spec": {
+           "finalizers": [
+           ]
+        },
+  
+EOF
+ansible-playbook replace-finalizer.yml -e namesapcename=$namespacename
+
+kubectl replace --raw "/api/v1/namespaces/tigera-system/finalize -f ${namespacename}.json
